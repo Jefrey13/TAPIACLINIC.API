5e4fdaa3b84e2992e41ea869ef1f1245b471ed2b
jefrey13 <jeff1zuniga3@gmail.com>
2024-11-29 03:06:25 -0600

    fix: Patient code fixed

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -10,8 +10,8 @@ public class User
     [Key]
     public int Id { get; set; }
 
-    [Required]
-    public string PatientCode { get; set; }
+    //[Required]
+    //public string PatientCode { get; set; }
 
     [Required]
     [MaxLength(100)]
